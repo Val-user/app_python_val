ef5d9267f857f372bb3453b6bee43b89a5d03cc8
Val-user <app_python_valero@outlook.com>
2024-07-29 08:35:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,11 @@ import pyodbc
 def init_dashboard(server):
     app = dash.Dash(
         __name__, 
-        server = app.server, 
+        server = server, 
         url_base_pathname='/dashboard/',
         external_stylesheets=[dbc.themes.BOOTSTRAP, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"]  # Ajout de FontAwesome
     )
+    server = app.server
     
     
     
