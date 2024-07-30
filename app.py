53659b54c772e3a6b82e1e371233343d56a4ac6c
Val-user <app_python_valero@outlook.com>
2024-07-30 13:30:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_ag_grid as dag
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import numpy as np
-import webbrowser
+import pymssql
 import json
 import os
 import dash_draggable
@@ -45,22 +45,18 @@ def init_dashboard():
     
         return df
     
-    # Connexion à la base de données MS Access
-    connection_string = (
-            "mssql+pyodbc://@DBSQLQCQCRF02/Laboratoire"
-            "?trusted_connection=yes&driver=SQL+Server+Native+Client+10.0"
-        )
-    engine = create_engine(connection_string)
-    
+    connection = pymssql.connect(
+        server='DBSQLQCQCRF02',
+        database='Laboratoire'
+    )
 
-    # Récupérer les données de la base de données
     query = '''
         SELECT NAIS_SAMPLES.SAMPLE_DATE, NAIS_SAMPLES.USER_SAMPLEID, NAIS_RESULTS.TESTID, NAIS_RESULTS.PROPERTYID, NAIS_RESULTS.NUMBER_VALUE
         FROM NAIS_SAMPLES
         INNER JOIN NAIS_RESULTS ON NAIS_SAMPLES.SAMPLE_ID = NAIS_RESULTS.SAMPLE_ID
         WHERE (((NAIS_SAMPLES.USER_SAMPLEID) Like '5069%') AND ((NAIS_RESULTS.NUMBER_VALUE) Is Not Null));
     '''
-    data = pd.read_sql(query, engine)
+    data = pd.read_sql(query, connection)
 
     
     
