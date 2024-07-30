a1232f903e557c002e1132d27634154eed5bd942
Val-user <app_python_valero@outlook.com>
2024-07-30 15:13:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,21 +45,23 @@ def init_dashboard():
     
         return df
     
-        connection_string = (
+    # Connexion à la base de données MS Access
+    connection_string = (
             "mssql+pyodbc://@DBSQLQCQCRF02/Laboratoire"
             "?trusted_connection=yes&driver=SQL+Server+Native+Client+10.0"
         )
-        engine = create_engine(connection_string)
+    engine = create_engine(connection_string)
     
 
     # Récupérer les données de la base de données
-        query = '''
+    query = '''
         SELECT NAIS_SAMPLES.SAMPLE_DATE, NAIS_SAMPLES.USER_SAMPLEID, NAIS_RESULTS.TESTID, NAIS_RESULTS.PROPERTYID, NAIS_RESULTS.NUMBER_VALUE
         FROM NAIS_SAMPLES
         INNER JOIN NAIS_RESULTS ON NAIS_SAMPLES.SAMPLE_ID = NAIS_RESULTS.SAMPLE_ID
         WHERE (((NAIS_SAMPLES.USER_SAMPLEID) Like '5069%') AND ((NAIS_RESULTS.NUMBER_VALUE) Is Not Null));
-        '''
-        data = pd.read_sql(query, engine)
+    '''
+    data = pd.read_sql(query, engine)
+
 
 
     
